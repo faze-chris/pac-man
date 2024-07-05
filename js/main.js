b9b6a1ddb2f153a43d2d963cc88ea7650f7c247f
Christian <118530836+faze-chris@users.noreply.github.com>
2024-07-05 16:49:29 +0200

    boundary start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-// canvas.width = innerWidth
-// canvas.height = innerHeight
-console.log(c)
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Boundary {
+    constructor({ position }) {
+        this.position = position;
+        this.width = 40;
+        this.height = 40;
+    }
+
+    draw() {
+        c.fillStyle = 'blue';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const boundary = new Boundary({ position: { x: 0, y: 0 } });
+boundary.draw();
