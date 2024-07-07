575f78034f738d815486e01b826da5721d4c5608
Christian <118530836+faze-chris@users.noreply.github.com>
2024-07-07 21:33:38 +0200

    i added some comment so that it is more easer to find stuf in my code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+// here I call my element "canvas".
 const canvas = document.querySelector('#gameCanvas');
 const c = canvas.getContext('2d');
 
+// this is the size of my map/game on my screen
 const size = Math.min(window.innerWidth, window.innerHeight) * 0.8;
 canvas.width = size;
 canvas.height = size;
 
+// this is where I set the boundary of how big and thick they are
 class Boundary {
     static width = 40;
     static height = 40;
@@ -15,17 +18,21 @@ class Boundary {
         this.image = image;
     }
 
+    // here I draw boundary
     draw() {
         c.drawImage(this.image, this.position.x, this.position.y);
     }
 }
 
+// this is a the player class / pacman
 class Player {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
-        this.radius = 18;
+        this.radius = 18; // the size of Pac-Man
     }
+
+    // here I draw the pacman
     draw() {
         c.beginPath();
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
@@ -33,18 +40,23 @@ class Player {
         c.fill();
         c.closePath();
     }
+
+    // this is for the pacman's speed
     update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
     }
-};
+}
 
+// this is a class for the pellets
 class Pellet {
     constructor({ position }) {
         this.position = position;
-        this.radius = 3;
+        this.radius = 3; // the size of a pellet
     }
+
+    // drawing the pellets
     draw() {
         c.beginPath();
         c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
@@ -52,10 +64,13 @@ class Pellet {
         c.fill();
         c.closePath();
     }
-};
+}
 
+// Arrays for storing pellets and obstacles
 const pellets = [];
 const boundaries = [];
+
+// here I create a player
 const player = new Player({
     position: {
         x: Boundary.width + Boundary.width / 2,
@@ -67,23 +82,17 @@ const player = new Player({
     }
 })
 
+// with this I set all keys to false so that pacman cannot move at the same time
 const keys = {
-    w: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    },
-    s: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
+    w: { pressed: false },
+    a: { pressed: false },
+    s: { pressed: false },
+    d: { pressed: false },
 }
 
 let lastKey = '';
 
+// this is the map of the game, this allows me to place where I want the walls and the pellets
 const map = [
     ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
     ['I', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', 'I'],
@@ -104,12 +113,14 @@ const map = [
     ['3', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '4'],
 ];
 
+// function to create images
 function createImage(src) {
     const image = new Image();
     image.src = src;
-    return image
+    return image;
 }
 
+// here I make the walls and the pellets for the game
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
         switch (symbol) {
@@ -200,7 +211,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/cl.png')
                     })
                 )
-                break
+                break;
             case 'R':
                 boundaries.push(
                     new Boundary({
@@ -211,7 +222,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/cr.png')
                     })
                 )
-                break
+                break;
             case '_':
                 boundaries.push(
                     new Boundary({
@@ -222,7 +233,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/cb.png')
                     })
                 )
-                break
+                break;
             case 'T':
                 boundaries.push(
                     new Boundary({
@@ -233,7 +244,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/ct.png')
                     })
                 )
-                break
+                break;
             case '+':
                 boundaries.push(
                     new Boundary({
@@ -244,7 +255,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/cps.png')
                     })
                 )
-                break
+                break;
             case '5':
                 boundaries.push(
                     new Boundary({
@@ -256,7 +267,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/pct.png')
                     })
                 )
-                break
+                break;
             case '6':
                 boundaries.push(
                     new Boundary({
@@ -268,7 +279,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/pcr.png')
                     })
                 )
-                break
+                break;
             case '7':
                 boundaries.push(
                     new Boundary({
@@ -280,7 +291,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/pcb.png')
                     })
                 )
-                break
+                break;
             case '8':
                 boundaries.push(
                     new Boundary({
@@ -291,7 +302,7 @@ map.forEach((row, i) => {
                         image: createImage('./img/L.png')
                     })
                 )
-                break
+                break;
             case '.':
                 pellets.push(
                     new Pellet({
@@ -301,31 +312,29 @@ map.forEach((row, i) => {
                         }
                     })
                 )
-                break
-            //  Add default case if needed
+                break;
             default:
                 break;
         }
     });
 });
 
-
-function circleCollidesWithRectangle({
-    circle,
-    rectang
-}) {
+// this is to check if the pac man crashes into the wall
+function circleCollidesWithRectangle({ circle, rectang }) {
     return (
         circle.position.y - circle.radius + circle.velocity.y <= rectang.position.y + rectang.height &&
         circle.position.x + circle.radius + circle.velocity.x >= rectang.position.x &&
         circle.position.y + circle.radius + circle.velocity.y >= rectang.position.y &&
         circle.position.x - circle.radius + circle.velocity.x <= rectang.position.x + rectang.width
     )
-};
+}
 
+// function for animation
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
 
+    // Here I check if there is input and if there is then the pac man will move
     if (keys.w.pressed && lastKey === 'w') {
         for (let i = 0; i < boundaries.length; i++) {
             const boundary = boundaries[i];
@@ -408,16 +417,19 @@ function animate() {
         }
     }
 
+    // here I draw the pellets
     for (let i = pellets.length - 1; 0 < i; i--) {
        const pellet = pellets[i];
        pellet.draw();
 
+       // here I check if the player has picked up a pellet
        if (Math.hypot(pellet.position.x - player.position.x,
            pellet.position.y - player.position.y) < pellet.radius + player.radius) {
            pellets.splice(i, 1);
        }
     };
    
+    // here I draw the boundary
     boundaries.forEach(boundary => {
         boundary.draw();
 
@@ -432,11 +444,14 @@ function animate() {
         };
     });
 
+    // here I call player update
     player.update();
 }
 
+// here I call animation
 animate()
 
+// this event listener for pressing keycaps
 addEventListener('keydown', ({ key }) => {
     switch (key) {
         case 'w':
@@ -458,6 +473,7 @@ addEventListener('keydown', ({ key }) => {
     }
 });
 
+// this event listener for releasing keycaps
 addEventListener('keyup', ({ key }) => {
     switch (key) {
         case 'w':
